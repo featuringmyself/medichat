89a3bc5ae8bd2c7716c6223208617702a7833eb1
Rudra <rudralocking@gmail.com>
2025-08-10 00:20:09 +0530

    feat: Enhance UI with new logo, title and hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import DropFile from "./DropFile";
 
 export default function Hero(){
     return (
-        <div className="h-screen flex items-center justify-evenly bg-[#DFE9FA]">
+        <div className="h-[85vh] flex items-center justify-evenly bg-[#DFE9FA]">
             <div className="max-w-xl">
                 <h1 className="text-6xl font-bold text-[#161554]">Reimagine your Health Reports with <span className="italic text-[#4548C4]">AI</span></h1>
                 <p className="mt-10 font-medium text-xl">Effortlessly understand, summarize, and chat about your medical reports.
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Navbar() {
     return (
-        <div className="mx-10 mt-4 bg-white rounded-2xl">
-            <Image src={"/logo.png"} width={1536} height={1024} alt="Logo" className="max-w-[8rem] h-auto"/>
+        <div className="mx-10 py-2 px-4 mt-4 bg-white rounded-2xl flex items-center justify-between">
+            <div className="flex items-center">
+                <Image src={"/logo.png"} width={1536} height={1024} alt="Logo" className="max-w-[4rem] h-auto" />
+                <h2 className="text-xl font-bold text-[#174e9b]">MediChat</h2>
+            </div>
         </div>
     )
 }
